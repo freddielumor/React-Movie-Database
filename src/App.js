69e5378cd44b3f59604e76bb4bb5d5b27a40bd22
freddie <Freddie@freds-mbp.home>
2018-06-03 19:31:16 +0100

    Updated app structure

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// Components
+
+class App extends Component {
+    constructor() {
+        super();
+        
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <h1>Movies test!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
-ReactDOM.render(
-    <div><App/></div>,
-    document.getElementById('app')
-);
-
-module.hot.accept();
+ReactDOM.render(<App />, document.getElementById('app'));
