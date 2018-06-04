48b0141aa926855d986a376cc7be71336af8de32
freddie <Freddie@freds-mbp.home>
2018-06-04 23:02:42 +0100

    App layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
 // Components
+import DefaultLayout from './Components/Layout/DefaultLayout';
 
 class App extends Component {
     constructor() {
@@ -13,16 +15,17 @@ class App extends Component {
 
     render() {
         return (
-            <div className="app">
-                <div className="container">
-                    <div className="row">
-                        <div className="col s12">
-                            <h1>Movies!</h1>
+            <Router>
+                <div className="app">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col s12">
+                                <DefaultLayout />
+                            </div>
                         </div>
                     </div>
                 </div>
-                
-            </div>
+            </Router>
         );
     }
 }
