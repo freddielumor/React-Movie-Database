9125dd3baa3eefc7c6d9f71edf7fb5bb02c5f880
Freddie Lumor <flumor@Freddie-Lumor.local>
2018-06-14 17:14:12 +0100

    switch from materialze grid to rsuite grid - WIP; Switch from css to sass files - wIP

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,37 +1,49 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Nav, Dropdown, Icon } from 'rsuite';
-import { Divider } from 'rsuite';
+import {
+    Grid,
+    Row,
+    Col,
+    Nav,
+    Dropdown,
+    Icon,
+    Divider
+} from 'rsuite';
+import './Header.scss';
+
 
 const Header = () => (
     <header className="app__header">
-        <div className="row">
-            <div className="col s12">
-                <h1 className="">React Movie Database!</h1>
-                <Divider />
-                <Nav>
-                    <Nav.Item 
-                        icon={<Icon icon="home" />}
-                        componentClass={Link}
-                        to="/"
-                    >
-                    Home
+        <Grid>
+            <Row>
+                <Col xs={24}>
+                    <h1 className="">React Movie Database</h1>
+                    <Divider className="app__divider" />
+                    <Nav>
+                        <Nav.Item
+                            icon={<Icon icon="home" />}
+                            componentClass={Link}
+                            to="/"
+                        >
+                            Home
                     </Nav.Item>
-                    <Nav.Item
-                        componentClass={Link}
-                        to="/new-releases"
-                    >
-                    New
+                        <Nav.Item
+                            componentClass={Link}
+                            to="/new-releases"
+                        >
+                            New
                     </Nav.Item>
-                    <Nav.Item
-                        componentClass={Link}
-                        to="/categories"
-                    >
-                    Categories
+                        <Nav.Item
+                            componentClass={Link}
+                            to="/categories"
+                        >
+                            Categories
                     </Nav.Item>
-                </Nav>
-            </div>
-        </div>
+                    </Nav>
+                </Col>
+            </Row>
+        </Grid>
+
     </header>
 );
 
