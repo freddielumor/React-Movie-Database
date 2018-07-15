d59074802febf2c242e2895b848fee4eb2e65291
freddie <Freddie@freds-mbp.home>
2018-07-15 02:47:34 +0100

    Retuen & display first 16 results, Pagination WIP

--- src/Components/Movies/MovieList.js
+++ src/Components/Movies/MovieList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'rsuite';
+import { Grid, Row, Col, Button } from 'rsuite';
 import axios from 'axios';
 
 // Components
@@ -12,27 +12,52 @@ class MovieList extends Component {
             initialApiUrl: 'https://api.themoviedb.org/3/discover/movie',
             apiUrl: 'https://api.themoviedb.org/3/search/movie',
             apiKey: 'e0c15850977d1058ff053d4726ac46f1',
-            movieList: []
+            movieList: [],
+            movieListPage: 1
         };
+        // this.loadNextPage = this.loadNextPage.bind(this);
+        // this.loadPreviousPage = this.loadPreviousPage.bind(this);
     }
 
     // Populate movie list on page load
     componentDidMount() {
         axios
             .get(
-                `${this.state.initialApiUrl}?api_key=${this.state.apiKey}&primaryreleasedate.gte=2016-01-01`
-            )
+            `${this.state.initialApiUrl}?api_key=${this.state.apiKey}&language=en-US&page=${this.state.movieListPage}&include_adult=false`)
             .then(res => this.setState({ movieList: res.data.results }))
-            .catch(err => console.log(err));
+            .catch(err => console.log(err))
+        
     }
+
+    // loadNextPage() {
+    //     axios
+    //         .get(
+    //         `${this.state.initialApiUrl}?api_key=${this.state.apiKey}&language=en-US&page=${this.state.movieListPage + 1}&include_adult=false`
+    //         )
+    //         .then(res => this.setState({ movieList: res.data.results }))
+    //         .catch(err => console.log(err));
+    // }
+
+    // loadPreviousPage() {
+    //     axios
+    //         .get(
+    //             `${this.state.initialApiUrl}?api_key=${this.state.apiKey}&language=en-US&page=${this.state.movieListPage - 1}&include_adult=false`
+    //         )
+    //         .then(res => this.setState({ movieList: res.data.results }))
+    //         .catch(err => console.log(err));
+    // }
 
     render() {
 
-        const { movieList } = this.state;
+        const { movieList, movieListPage } = this.state;
         console.log({ movieList });
+        const filteredMovieList = movieList.slice(1, 17);
+        console.log({ filteredMovieList });
+        // console.log({ movieListPage });
+        
 
         // Map over results & return data
-        let movieListMapped = movieList.map((item, index) => {
+        let movieListMapped = filteredMovieList.map((item, index) => {
             return (
                 <Col xs={24} sm={12} md={6} key={index}>
                     <MovieCard
@@ -55,8 +80,41 @@ class MovieList extends Component {
                     <Row>
                         {movieListMapped}
                     </Row>
+                    {/* <Row>
+                        <Col xs={24}>
+                            {movieListPage >= 1 &&
+                            <React.Fragment>
+                                <Button 
+                                    appearance="primary"
+                                    onClick={this.loadNextPage}
+                                >
+                                    NEXT PAGE
+                                </Button>
+
+                                <Button
+                                    appearance="primary"
+                                    onClick={this.loadPreviousPage}
+                                >
+                                    PREVIOUS PAGE
+                                </Button>
+                            </React.Fragment>
+                            }
+
+                            {movieListPage.length <= 1 &&
+                                <React.Fragment>
+
+                                    <Button
+                                        appearance="primary"
+                                onClick={this.loadNextPage}
+                                    >
+                                        NEXT PAGE
+                                </Button>
+                                </React.Fragment>
+                            }
+                        </Col>
+                    </Row> */}
                 </Grid>
-            </div >
+            </div>
         )
     }
 }
