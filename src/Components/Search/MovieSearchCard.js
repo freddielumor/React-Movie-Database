818f4928c4425a2ef1a8ff17ae9574eae24a1004
Freddie Lumor <flumor@Freddie-Lumor.local>
2018-07-16 12:14:26 +0100

    Create search result component

---
+++ src/Components/Search/MovieSearchCard.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Grid, Row, Col } from 'rsuite';
+import { Link } from 'react-router-dom';
+import './MovieSearchCard.scss';
+
+class MovieSearchCard extends Component {
+    constructor() {
+        super();
+        this.state = {
+        };
+    }
+
+    render() {
+        const { id, image, title, description, releaseDate } = this.props;
+        return (
+            <Link to={`/movie/${id}`} className="movie-search-card" id={id}>
+                <img src={`https://image.tmdb.org/t/p/w342${image}`} />
+                <div className="movie-search-card__details">
+                    <h3 className="movie-search-card__details-title">{title}</h3>
+                    <h4 className="movie-search-card__details-release-date">{releaseDate}</h4>
+                </div>
+            </Link>
+        )
+    }
+}
+
+export default MovieSearchCard;
