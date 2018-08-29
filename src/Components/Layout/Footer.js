3f696003ae5e0d489b31e805471257a5e37156d6
freddie <Freddie@freds-mbp.home>
2018-08-29 22:43:35 +0100

    Update footer

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Grid, Row, Col } from 'rsuite';
+import './Footer.scss';
 
 const Footer = () => (
     <footer className="app__footer">
         <Grid>
             <Row>
                 <Col xs={24}>
-                    <p>React Movie Database. Data from the TMBD API </p>
+                    <p>Data from the <a href="https://www.themoviedb.org/" target="_blank"> TMBD API</a>.  Made by <a className="app__footer--freddie" href="https://github.com/freddielumor" target="_blank">Freddie</a></p>
                 </Col>
             </Row>
         </Grid>
