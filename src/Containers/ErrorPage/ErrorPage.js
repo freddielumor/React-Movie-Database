8262002b1ec00c02977bc7806ee6fcf135e6c3e4
freddie <Freddie@freds-mbp.home>
2018-09-01 01:12:19 +0100

    Error page component

---
+++ src/Containers/ErrorPage/ErrorPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './ErrorPage.scss';
+
+const ErrorPage = props => (
+    <main className="app__error">
+        <h1>PAGE NOT FOUND</h1>
+
+        <div className="app__error--link">
+            <Link to={'/'}>
+                HOMEPAGE
+             </Link>
+        </div>
+    </main >
+);
+
+export default ErrorPage;
