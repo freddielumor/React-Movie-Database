af78aa1592ee364678fcd8fa17803e36efd28a1a
freddie <Freddie@freds-mbp.home>
2018-09-01 14:27:15 +0100

    Update dist path

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: __dirname + '/dist',
-        publicPath: '',
+        publicPath: '/dist/',
         filename: 'bundle.js'
     },
     mode: 'development',
