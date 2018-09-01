4a819f1490c735bbb21aa2431ba43c2b40fc0b02
freddie <Freddie@freds-mbp.home>
2018-09-01 14:00:27 +0100

    Switch to hash router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { Router } from 'react-router-dom';
+import createHashHistory from 'history/createHashHistory';
 import App from './App';
 import './index.scss';
 
-ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, document.getElementById('root'));
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
+
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <App />
+    </Router>,
+    document.getElementById('root')
+);
