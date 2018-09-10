6e93291f5b16f955fd9daddafcb91e57ddf796da
Freddie Lumor <flumor@Freddie-Lumor.local>
2018-09-10 12:22:47 +0100

    Fix redux store - add logger, conditionally remove devtools extension if safari

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,15 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { logger } from 'redux-logger';
 import rootReducer from './Reducers';
 
 const initialState = {};
+
 const store = createStore(
     rootReducer,
     initialState,
     compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+        applyMiddleware(thunk, logger),
+        window.navigator.userAgent.includes('Chrome') ?
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose,
+    ),
 );
 
 export default store;
