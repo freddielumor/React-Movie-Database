9929fe98ba5a86bc617fd774edc11d2f17c84fd5
Freddie Lumor <flumor@Freddie-Lumor.local>
2018-09-10 11:59:08 +0100

    Update passing of thunk in store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,12 +3,11 @@ import thunk from 'redux-thunk';
 import rootReducer from './Reducers';
 
 const initialState = {};
-const middleware = [thunk];
 const store = createStore(
     rootReducer,
     initialState,
     compose(
-        applyMiddleware(...middleware),
+        applyMiddleware(thunk),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
