a23f33c9690336a35f0fb7f7e9e2b51aaa0cee47
Freddie Lumor <flumor@freddie-lumor.home>
2018-10-07 18:52:29 +0100

    Update cast componenent

--- src/Components/Movies/MovieCast.js
+++ src/Components/Movies/MovieCast.js
@@ -1,62 +1,19 @@
 import React, { Component } from 'react';
-import Swiper from 'react-id-swiper';
 import './MovieCast.scss';
 
 class MovieCast extends Component {
     constructor(props) {
         super(props);
-        this.goNext = this.goNext.bind(this)
-        this.goPrev = this.goPrev.bind(this)
-    }
-
-    goNext() {
-        if (this.swiper) this.swiper.slideNext()
-    }
-
-    goPrev() {
-        if (this.swiper) this.swiper.slidePrev()
     }
 
     render() {
-        const { name, character, image, item, movieCredits } = this.props;
-        const params = {
-            slidesPerView: 3,
-            spaceBetween: 10,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev'
-            },
-            breakpoints: {
-                600: {
-                    slidesPerView: 2,
-                    spaceBetween: 20
-                },
-                480: {
-                    slidesPerView: 1,
-                    spaceBetween: 10
-                }
-            }
-        };
+        const { id, name, character, profile_path } = this.props;
 
         return (
-            <div className="movie-cast">
-
-                {movieCredits.cast.slice(0, 10).map((item, index) => {
-                    return (
-
-                        <Swiper {...params}>
-                            <div key={index}>
-                                <img src={`https://image.tmdb.org/t/p/w185${item.profile_path}`} alt={`${item.name} image`} />
-                                <p>{item.name}</p>
-                                <p>{item.character}</p>
-                            </div>
-                        </Swiper>
-                    )
-                })}
+            <div className="movie-cast swiper-slide" id={id}>
+                <img src={`https://image.tmdb.org/t/p/w185${profile_path}`} alt={`${name} image`} />
+                <p>{name}</p>
+                <p>{character}</p>
             </div>
         )
     }
