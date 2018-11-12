32e9a92599ddedfa4a70e0048f54b72dd047c27b
freddie <Freddie@freds-mbp.home>
2018-11-12 21:39:20 +0000

    Update movie cast component

--- src/Components/Movies/MovieCast.js
+++ src/Components/Movies/MovieCast.js
@@ -10,11 +10,11 @@ class MovieCast extends Component {
         const { id, name, character, profile_path } = this.props;
 
         return (
-            <div className="movie-cast swiper-slide" id={id}>
+            <figure className="movie-cast" id={id}>
                 <img src={profile_path === null ? 'http://via.placeholder.com/300x450' : `https://image.tmdb.org/t/p/w185${profile_path}`} alt={`${name} image`} />
-                <p>{name}</p>
-                <p>{character}</p>
-            </div>
+                <figcaption>{name}</figcaption>
+                <figcaption>{character}</figcaption>
+            </figure>
         )
     }
 }
