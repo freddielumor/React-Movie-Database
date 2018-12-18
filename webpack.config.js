1ba615aa286fc993fd1e4835ab55dfeb214347e1
flumor <flumor@brilliantbasics.com>
2018-12-18 13:48:24 +0000

    Update webpack public path

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: __dirname + '/public',
-        publicPath: '/',
+        publicPath: '',
         filename: 'bundle.js'
     },
     mode: 'development',
