a09b32708a00f2ece420ec6235cbe794a212a02e
Fred Lumor <fredlumor@freds-mbp.home>
2020-02-09 22:14:11 +0000

    Remove redux logger

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-// import { logger } from "redux-logger";
 import rootReducer from "./Reducers";
 
 const initialState = {};
